4c6e5ee19d23b1efb63a74fc203e0e43afc45666
ThiagoROX <thiagokenis@live.com>
2025-07-18 22:12:19 -0300

    Import Locale in SubCommand_Sign.java
    Added import for java.util.Locale in SubCommand_Sign.java to support locale-specific operations. No other changes were made.

--- quickshop-bukkit/src/main/java/com/ghostchu/quickshop/command/subcommand/SubCommand_Sign.java
+++ quickshop-bukkit/src/main/java/com/ghostchu/quickshop/command/subcommand/SubCommand_Sign.java
@@ -14,6 +14,7 @@ import org.jetbrains.annotations.NotNull;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.Locale;
 import java.util.Set;
 
 public class SubCommand_Sign implements CommandHandler<Player> {
